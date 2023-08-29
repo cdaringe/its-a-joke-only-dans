a7b72b28f68d0c8b1712c75805b6d7db40a0b37b
cdaringe <cdaringe@gmail.com>
2023-08-29 00:08:53 -0700

    flatten

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  // @todo enable for hosting on github.io domain, not onlydans.lol
+  // basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
 };
 
 module.exports = nextConfig;
